53941cccdb37933c49a788d616cbbd8b35e3bcb0
Bagus Edi Fathorrasi <bagusbangselok@gmail.com>
2026-08-20 09:10:01 +0700

    Update App.php untuk tunnel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Percayai semua proxy (Cloudflare Tunnel)
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
             'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
