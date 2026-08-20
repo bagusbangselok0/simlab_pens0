47114e6a69b8cdc5b1979a33d51db49a541a76a5
Bagus Edi Fathorrasi <bagusbangselok@gmail.com>
2026-08-20 09:26:25 +0700

    Update App.php untuk tunnel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Carbon::setLocale('id');
+        // Paksa HTTPS jika diakses via Cloudflare / production
+        if (app()->environment('production') || request()->header('x-forwarded-proto') === 'https') {
+            URL::forceScheme('https');
+        }
     }
 }
